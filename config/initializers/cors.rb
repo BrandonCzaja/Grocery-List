9c6235dfe20e6ed910c88fc534d4df3665bd8c71
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-12-12 16:45:07 -0600

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bc-grocery-list.herokuapp.com/'
+    origins 'https://pedantic-almeida-939a03.netlify.app/'
 
     resource '*',
       headers: :any,
