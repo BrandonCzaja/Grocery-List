323086da172192f717c8689ba9943e59118821f9
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-12-12 16:40:29 -0600

    Fixed cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bc-grocery-list.herokuapp.com'
+    origins 'https://bc-grocery-list.herokuapp.com/'
 
     resource '*',
       headers: :any,
